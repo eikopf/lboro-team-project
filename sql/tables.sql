d30b1bc846cf6406824318262fc8f6961a108651
Oliver Wooding <oliverwooding@icloud.com>
2024-04-19 15:59:37 +0200

    added notes and explanations to tables.sql

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,9 +1,18 @@
--- this script creates 3 tables in the current database, which roughly
--- correspond to the tables given in the coursework outline
+-- this script creates 3 tables in the current database, which roughly correspond to the tables given in the coursework outline
 
 -- refer to https://dev.mysql.com/doc/refman/8.3/en/create-table.html#create-table-options for
--- more details, but broadly speaking MyISAM is the "almost always read" table setting, and
--- has been used for this reason
+-- more details, but broadly speaking: MyISAM is the "almost always read" table setting, and
+-- has been used as the engine for this reason
+
+-- DESIGN NOTES:
+-- 1. all tables have `id` as their first field and primary key
+-- 2. arbitrary text fields are either short (varchar(20)), long (varchar(40)), or unbounded (longtext)
+-- 3. foreign keys are explicitly defined with the foreign key directive
+-- 4. where possible (e.g. in users.rating) types are strictly constrained
+
+-- OPEN QUESTIONS:
+-- 1. should items.category be a different type? i feel that it might be better to have a distinct two-column "category" table that associates item ids with categories
+-- 2. what does the version of mysql on the group server support? it appears to be an archaic version of MariaDB, but i can't find anything else of note
 
 create table users (
   id int not null auto_increment,
@@ -23,7 +32,7 @@ create table users (
 
 create table items (
   id int not null auto_increment,
-  owner int(20) not null,
+  owner int not null,
   title varchar(40) not null,
   category varchar(40) not null,
   description longtext not null,
