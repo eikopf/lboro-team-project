e75f1e7dc288266689f68a41075fe0172b0f742a
Oliver Wooding <oliverwooding@icloud.com>
2024-04-27 21:16:04 +0100

    updated table schemata

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,46 +4,48 @@
 -- more details, but broadly speaking: MyISAM is the "almost always read" table setting, and
 -- has been used as the engine for this reason
 
--- DESIGN NOTES:
+-- NOTE:
 -- 1. all tables have `id` as their first field and primary key
 -- 2. arbitrary text fields are either short (varchar(20)), long (varchar(40)), or unbounded (longtext)
 -- 3. foreign keys are explicitly defined with the foreign key directive
 -- 4. where possible (e.g. in users.rating) types are strictly constrained
 
--- OPEN QUESTIONS:
--- 1. should items.category be a different type? i feel that it might be better to have a distinct two-column "category" table that associates item ids with categories
--- 2. what does the version of mysql on the group server support? it appears to be an archaic version of MariaDB, but i can't find anything else of note
-
 create table users (
   id int not null auto_increment,
   email varchar(20) not null,
   name varchar(20) not null,
-
-  -- !!!!! THIS IS A TERRIBLE IDEA IN REAL CODE !!!!!
+  -- passwords are stored in plaintext
   password varchar(20) not null,
-  -- !!!!! SERIOUSLY, DON'T EVER STORE PASSWORDS IN PLAINTEXT !!!!!
-
   address varchar(20) not null,
   postcode varchar(20) not null,
-  -- a rating is a number from 0 to 100 (inclusive)
-  rating tinyint(1) unsigned not null default 0 check (rating <= 100),
+  -- ratings are bounded from 0 to 100
+  -- NOTE: the version of mysql on the servers does not support CHECK constraints
+  rating tinyint unsigned not null default 0 check (rating <= 100),
   primary key (id)
 ) engine = MyISAM;
 
 create table items (
   id int not null auto_increment,
+  -- a foreign key referencing the owner's user-id
   owner int not null,
   title varchar(40) not null,
-  category varchar(40) not null,
+  -- categories are packed into a bitset
+  categories set(
+    'art',
+    'books', 
+    'clothes',
+    'electronics', 
+    'media',
+    'music',
+    'toys'
+  ) not null,
   description text not null,
   price decimal(6, 2) not null,
   postage varchar(40) not null,
   start timestamp not null default current_timestamp on update current_timestamp,
   finish timestamp not null,
   primary key (id),
-  -- an item must always be owned by a user
-  foreign key (owner) references users(id),
-  FULLTEXT (title, description)
+  foreign key (owner) references users(id)
 ) engine = MyISAM;
 
 create table images (
